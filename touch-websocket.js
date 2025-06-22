f160346c6e486b85bd332f8cea3df0c04f08e961
dauuoon <dauuoon@gmail.com>
2025-06-23 02:12:59 +0900

    TouchDesigner 웹소켓 연동 및 인터랙티브 페이지 구현

--- touch-websocket.js
+++ touch-websocket.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://211.38.170.175'); // 실제로는 TouchDesigner가 실행 중인 PC의 IP로 변경
+const ws = new WebSocket('ws://211.38.170.175:8765'); // 실제로는 TouchDesigner가 실행 중인 PC의 IP로 변경
 
 ws.onopen = () => {
   console.log('웹소켓 연결됨!');
