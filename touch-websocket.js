c2ded72de4d4d6a5511eb9e7ea353d635aa7e8ba
dauuoon <dauuoon@gmail.com>
2025-06-23 02:42:31 +0900

    WebSocket 주소 wss 프록시로 변경 및 중복 선언 제거(Mixed Content 해결)

--- touch-websocket.js
+++ touch-websocket.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket('ws://211.38.170.175:8765'); // 실제로는 TouchDesigner가 실행 중인 PC의 IP로 변경
+const ws = new WebSocket('wss://211.38.170.175:8443'); // 프록시 서버의 WSS 주소로 변경
 
 ws.onopen = () => {
   console.log('웹소켓 연결됨!');
